a763f46371fedccbf03bb1cd16e275fdc3c59a0a
mayue13 <mayurdahak13@gmail.com>
2022-06-15 20:23:08 +0530

    depolu

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -16,7 +16,7 @@ export default {
               apiId: '3e340097-eb3a-4b14-9664-199a6f144f1a',
               buildHookId: '62a9ebb1b4f70e33f68113c7',
               name: 'exploremd',
-              url: 'https://my-sanity-deployment.com',
+              url: 'https://eploremd.netlify.app/',
             }
           ]
         }
