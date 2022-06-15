6789b118bdec4418b9e3ed1c9ae38c8b46040610
mayue13 <mayurdahak13@gmail.com>
2022-06-15 19:56:29 +0530

    sanity

---
+++ src/dashboardConfig.js
@@ -0,0 +1,25 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'Porfolio deploys',
+          sites: [
+            {
+              title: 'Sanity Studio',
+              apiId: '3e340097-eb3a-4b14-9664-199a6f144f1a',
+              buildHookId: '62a9ebb1b4f70e33f68113c7',
+              name: 'sanity-gatsby-blog-20-studio',
+            },
+            {
+              title: 'Website',
+              apiId: '3e340097-eb3a-4b14-9664-199a6f144f1a',
+              buildHookId: '62a9ebb1b4f70e33f68113c7',
+              name: 'exploremd',
+              url: 'https://my-sanity-deployment.com',
+            }
+          ]
+        }
+      }
+    ]
+  }
